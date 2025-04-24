9a4069fa39b85c7ed438e4e8fd74cb9a735ed3d0
darthspectrum <muradianator@gmail.com>
2025-04-25 00:46:43 +0400

    :boom: unable to handle calls that are resolved at a parent level and return a primitive

--- oopfp/lab.py
+++ oopfp/lab.py
@@ -18,6 +18,7 @@ def account(balance):
       return balance
     else:
       print("wat?")
+      return "wat?"
 
   return dispatch
 
@@ -40,4 +41,45 @@ def savings_account(balance):
 
 print(savings_account(60)('deposit', 60)('interest'))
 
+def credit_account(balance):
+  baccount = account(balance)
+  saccount = savings_account(balance)
+
+  def dispatch(action, param = ""):
+    if(action == balance):
+      return balance
+    else:
+      sa_result = saccount(action, param)
+
+      if(sa_result == 'wat?'):
+        ba_result = baccount(action, param)
+
+        if(ba_result != 'wat?'):
+          new_credit_account = \
+            credit_account(ba_result('balance'))
+          return new_credit_account
+        else:
+          return f"unable to resolve {action}"
+      else:
+        new_credit_account = \
+          credit_account(sa_result('balance'))
+        return new_credit_account
+
+  return dispatch
+
+print(credit_account(100)('balance'))
+
 # print(account(160)('balance',10)) # --> 60
+
+class A:
+  def a(self):
+    return 10
+
+class B:
+  def a(self):
+    return 20
+
+class C(A, B):
+  pass
+
+# print(C.mro())
