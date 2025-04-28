2a26559ddca4a26c3d7eb3e7d68dc4314f67a4df
darthspectrum <muradianator@gmail.com>
2025-04-28 12:35:07 +0400

    :notebook: more on todays lab

--- oopfp/lab.py
+++ oopfp/lab.py
@@ -15,7 +15,11 @@ def account(*params):
     # we need extract to be able to typecast the params /instantiate the child object/
     if(action == 'extract'):
       return params
-    if(action == 'deposit'):
+    # we need to identify the type of account, since when we return new functions
+    # we are unable to compare them with ==
+    elif (action == 'identify'):
+      return 'account'
+    elif(action == 'deposit'):
       return account(deposit(param))
     elif (action == 'withdraw'):
       return account(withdraw(param))
@@ -46,14 +50,14 @@ def savings_account(balance):
 print(savings_account(60)('deposit', 60)('interest'))
 
 def credit_account(balance):
-  baccount = account(balance)
-  saccount = savings_account(balance)
+  basic_account = account(balance)
+  savings_account = savings_account(balance)
 
   def dispatch(action, param = ""):
-    sa_result = saccount(action, param)
+    sa_result = savings_account(action, param)
 
     if(sa_result == 'wat?'):
-      ba_result = baccount(action, param)
+      ba_result = basic_account(action, param)
 
       if(ba_result != 'wat?'):
         new_credit_account = \
@@ -77,7 +81,7 @@ def credit_account(balance):
 #     print(credit_account(100)('balance'))
 #           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #   File "/home/spectrum/playground/functions/oopfp/lab.py", line 52, in dispatch
-#     sa_result = saccount(action, param)
+#     sa_result = savings_account(action, param)
 #                 ^^^^^^^^^^^^^^^^^^^^^^^
 #   File "/home/spectrum/playground/functions/oopfp/lab.py", line 37, in dispatch
 #     savings_account(basic_account(action, param)('balance'))
@@ -91,7 +95,9 @@ def type_respecting_savings_account(balance):
   basic_account = account(balance)
 
   def dispatch(action, param=""):
-    if(action == 'interest'):
+    if (action == 'identify'):
+      return 'savings_account'
+    elif(action == 'interest'):
       return balance * 0.2
     elif (action == 'deposit'):
       return type_respecting_savings_account(balance + 2 * param)
@@ -108,38 +114,51 @@ def type_respecting_savings_account(balance):
   return dispatch
 
 def type_respecting_credit_account(balance):
-  baccount = account(balance)
-  saccount = type_respecting_savings_account(balance)
+  basic_account = account(balance)
+  savings_account = type_respecting_savings_account(balance)
+
+  def pnd(): return None
 
   def dispatch(action, param = ""):
-    sa_result = saccount(action, param)
+    sa_result = savings_account(action, param)
 
-    if(sa_result == 'wat?'):
-      ba_result = baccount(action, param)
-
-      if(ba_result != 'wat?'):
-        if ba_result == account:
-          new_credit_account = \
-            type_respecting_credit_account(baccount(action, param)('balance'))
-          return new_credit_account
-        else:
-          return ba_result
-      else:
-        return f"unable to resolve {action}"
+    if(action == 'identify'):
+      return 'credit_account'
     else:
-      if sa_result == type_respecting_savings_account:
-        new_credit_account = \
-          type_respecting_credit_account(saccount('balance'))
-        return new_credit_account
-      else :
-        return sa_result
+      if(sa_result == 'wat?'):
+        ba_result = basic_account(action, param)
+
+        if(ba_result != 'wat?'):
+          if ba_result == account:
+            new_credit_account = \
+              type_respecting_credit_account(basic_account(action, param)('balance'))
+            return new_credit_account
+          else:
+            return ba_result
+        else:
+          return f"unable to resolve {action}"
+      else:
+        if sa_result == pnd:
+          new_credit_account = \
+            type_respecting_credit_account(savings_account('balance'))
+          return new_credit_account
+        else :
+          return sa_result
 
   return dispatch
 
-print(type_respecting_credit_account(120)('balance'))
+print(type_respecting_credit_account(100)('deposit', 20)('withdraw', 60)('balance'))
 
 # This assumes the dependencies be of same _type_, or at least accept the args
 def a_more_convenient_account_constructor(*args, dependencies = []):
+
+  def own_dispatch(action, *params):
+    if action == 'identify':
+      return 'a_more_convenient_account'
+    if action == 'report':
+      return f"Current balance is {args[0]} USD"
+    else:
+      return 'wat?'
 
   def dispatch(action, *params):
     # one by one instantiate the dependencies with the params
@@ -149,22 +168,24 @@ def a_more_convenient_account_constructor(*args, dependencies = []):
     # - the last result
     # - something of your own
 
+    if own_dispatch(action, *params) != 'wat?':
+      return own_dispatch(action, *params)
+
     for dep in dependencies:
-      result = dep(*args)(action, *params)
+      dep_at_hand = dep(*args)
+      result = dep_at_hand(action, *params)
       if result != 'wat?':
-        if result == dep:
+        # if result == dep_at_hand: This branch will not work because
+        if callable(result) and result('identify') == dep_at_hand('identify'):
           # if the result is the same as the dependency, we need to
           # create a new instance of the dependency with the params
-          return a_more_convenient_account_constructor(*dep('extract'))
+          return a_more_convenient_account_constructor(*result('extract'))
         else:
           return result
 
-    return 'wat?'
-
-
   return dispatch
 
-print(a_more_convenient_account_constructor(320, dependencies=[type_respecting_savings_account, account])('deposit', 50)('balance'))
+print(a_more_convenient_account_constructor(320, dependencies=[type_respecting_savings_account, account])('deposit', 50)('report'))
 
 # misc
 
@@ -180,3 +201,22 @@ class C(A, B):
   pass
 
 # print(C.mro())
+
+f1 = lambda x: x + 1
+f2 = lambda x: x + 1
+
+if f1 == f2:
+  print("yes")
+else:
+  print("no")
+
+def hof(a):
+  def f(b):
+    return a + b
+
+  return f
+
+if hof(5) == hof(5):
+  print("yes")
+else:
+  print("no")
