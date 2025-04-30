4b0db5e3b21817758b2f70cc083dece17d613b92
darthspectrum <muradianator@gmail.com>
2025-04-30 13:23:26 +0400

    :notebook: somewhat universal account creator

--- oopfp/todays-lab.py
+++ oopfp/todays-lab.py
@@ -2,7 +2,9 @@
 # f(5)
 # (a) (a + b) env
 
-def account(balance):
+def account(*args):
+  balance = args[0]
+
   def deposit(amount):
     return balance + amount
 
@@ -13,7 +15,7 @@ def account(balance):
     if action == 'id':
       return 'basic_account'
     elif action == 'extract':
-      return balance
+      return args
     elif(action == 'deposit'):
       return account(deposit(param))
     elif (action == 'withdraw'):
@@ -85,28 +87,75 @@ print(credit_account(100)('deposit', 20)('balance'))
 # print(account(160)('balance',10)) # --> 60
 
 def convenient_account(balance, dependencies=[]):
-  def own_dispatch():
-    pass
 
+  def own_dispatch(action, params=''):
+    if action == 'report':
+      return f'Your account balance is {balance} USD.'
+    elif action == 'id':
+      return 'convenient_account'
+    else:
+      return 'wat?'
+
+  # one by one instantiate the dependencies with the args
+  # see if any of them can handle the action. Once one of them can
+  # handle the action, return the result
+  # if none of them can handle the action, return
+  # - the last result
+  # - something of your own
   def dispatch(action, params=''):
-    # one by one instantiate the dependencies with the args
-    # see if any of them can handle the action. Once one of them can
-    # handle the action, return the result
-    # if none of them can handle the action, return
-    # - the last result
-    # - something of your own
-    for dependency in dependencies:
-      dep_result = dependency(balance)(action, params)
-      if dep_result != 'wat?':
-        if callable(dep_result) and dep_result('id') == dependency('id'):
-          state = dep_result('extract')
-          return convenient_account(state, dependencies)
-        else:
-          return dep_result
+    own_result = own_dispatch(action, params)
+    if own_result != 'wat?':
+      return own_result
+    else:
+      for dependency in dependencies:
+        dep_at_hand = dependency(balance)
+        dep_result = dep_at_hand(action, params)
+        if dep_result != 'wat?':
+          if callable(dep_result) and dep_result('id') == dep_at_hand('id'):
+            state = dep_result('extract')
+            return convenient_account(state, dependencies)
+          else:
+            return dep_result
 
   return dispatch
 
-convenient_account(100, [account, savings_account])('deposit', 20)('balance')
+print(convenient_account(100, [savings_account, account])('deposit', 30)('balance'))
+print(convenient_account(100, [savings_account, account])('deposit', 130)('report'))
+
+def somewhat_universal_account_constructor(*args, dependencies=[]):
+
+  def own_dispatch(action, *params):
+    if action == 'id':
+      return 'somewhat_universal_object_constructor'
+    elif action == 'report':
+      return f'You have {args[0]} on this account.'
+    else:
+      return 'wat?'
+
+  # one by one instantiate the dependencies with the args
+  # see if any of them can handle the action. Once one of them can
+  # handle the action, return the result
+  # if none of them can handle the action, return
+  # - the last result
+  # - something of your own
+  def dispatch(action, *params):
+    own_result = own_dispatch(action, *params)
+    if own_result != 'wat?':
+      return own_result
+    else:
+      for dependency in dependencies:
+        dep_at_hand = dependency(*args)
+        dep_result = dep_at_hand(action, *params)
+        if dep_result != 'wat?':
+          if callable(dep_result) and dep_result('id') == dep_at_hand('id'):
+            state = dep_result('extract')
+            return convenient_account(*state, dependencies)
+          else:
+            return dep_result
+
+  return dispatch
+
+print(somewhat_universal_account_constructor(100, dependencies=[savings_account, account])('deposit', 80)('report'))
 
 class A:
   def a(self):
